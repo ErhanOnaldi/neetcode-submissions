8bd72ebdd1acaa586e88155a89297a7bc9b4da90
Erhan Önaldı <165405003+ErhanOnaldi@users.noreply.github.com>
2026-05-12 14:01:13 +0300

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def houseRobber(arr):
+            prev2 = 0
+            prev1 = 0
+
+            for num in arr:
+                curr = max(prev1, num + prev2)
+                prev2 = prev1
+                prev1 = curr
+
+            return prev1
+
+        return max(
+            houseRobber(nums[:-1]),
+            houseRobber(nums[1:])
+        )
